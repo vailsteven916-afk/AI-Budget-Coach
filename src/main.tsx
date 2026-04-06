b0e1bc80c1145ac97e66944b209a6a4bddd8745f
vailsteven916-afk <vailsteven916@gmail.com>
2026-04-07 00:21:25 +0600

    feat: Configure RevenueCat SDK
    Initialize the RevenueCat SDK with a test API key and an anonymous app user ID upon application startup. This enables basic purchase tracking and management functionality.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import { Purchases } from "@revenuecat/purchases-js";
 import App from './App.tsx';
 import './index.css';
+
+const appUserId = Purchases.generateRevenueCatAnonymousAppUserId();
+Purchases.configure({
+    apiKey: "test_munXrmSXbnatIsKCHTECDDdOBMr",
+    appUserId: appUserId,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
